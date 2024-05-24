03b17d2b157b23bf7a3b662551d14529aba47579
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-05-24 00:17:43 -0500

    commit 8

---
+++ grafica/js/funciones.js
@@ -0,0 +1,10 @@
+function mostrar(){
+    const x = document.getElementById('sidebar');
+    
+    if(x.style.display === 'none'){
+        x.style.display = 'flex';
+        TransitionEvent
+    }else{
+        x.style.display='none';
+    }
+}
