d64c22f95b42f9600e0c674c170241e6582213a6
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-05-31 22:05:55 -0500

    Customer deleted

--- src/main/java/com/api/crud/repository/ClientDAO.java
+++
@@ -1,9 +0,0 @@
-package com.api.crud.repository;
-
-import com.api.crud.entity.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClientDAO extends JpaRepository<Cliente, Long> {
-}
